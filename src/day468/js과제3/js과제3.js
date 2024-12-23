c20591a8a2fd58cb1bf5a42f7fa67d59c3bcaaa3
UnitePdk <qkrrjs1515@gmail.com>
2024-12-23 21:46:50 +0900

    1223

--- src/day468/js과제3/js과제3.js
+++ src/day468/js과제3/js과제3.js
@@ -60,18 +60,7 @@ setSeat(seat4);
 setSeat(seat5);
 setSeat(seat6);
 
-/*
-[문제3]학생들의 점수 출력하는 구현 
-    1. 학생이름배열 선언 
-        let nameArray = [ '유재석' , '강호동' , '신동엽' ]
-    2. 학생점수배열 선언 
-        let scores = [ 92 , 86 , 72 ]
-    3. 아래와 출력 , 100점 만점 이라고 했을때 점수를 ● 표기 나머지 점수 ○
-        이름    점수 시각화 
-        유재석  ●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●○○○○○○○
-        강호동  ●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●○○○○○○○○○○○○
-        신동엽  ●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●○○○○○○○○○○○○○○○○○○
-*/
+// [문제 3]
 let nameArray = ["유재석", "강호동", "신동엽"];
 let scores = [92, 86, 72];
 
@@ -99,3 +88,29 @@ student3.innerHTML = nameArray[2];
 score1.innerHTML = printScore(scores[0]);
 score2.innerHTML = printScore(scores[1]);
 score3.innerHTML = printScore(scores[2]);
+
+/*
+// [문제4]
+- 조건1 : 배열내 중복 값 은 없어야 한다.
+- 조건2 : 1 부터 45 사이의 값만 입력받는다. 
+- 조건3 : 총 6개의 번호만 입력받는다.
+- 입력조건 : prompt()함수 출력 
+- 출력조건 : 특정 div에 입력받은 로또 번호 6개를 출력하기.
+*/
+
+lotto = document.querySelector(".lotto");
+let lottoArray = [];
+
+for (let i = 0; i < 6; i++) {
+  const num = prompt("1~45 번호 입력");
+  if (num > 45 || num < 1) {
+    alert("경고: 1~45 사이 값만 입력");
+    return;
+  }
+  if (lottoArray.includes(num)) {
+    alert("경고: 이미 등록한 번호");
+    return;
+  }
+  lottoArray.push(num);
+}
+lotto.innerHTML = lottoArray;
